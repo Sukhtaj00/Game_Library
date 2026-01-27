e6498fcc7d57157449686e7cecb10974af218450
Sukhtaj Dhillon <you@example.com>
2026-01-27 09:29:27 -0600

    updated tsx file

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import GameCollection from "./components/game_collection/game_collection";
+import GameProgress from "./components/game_progress/game_progress";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="app">
+      <header>
+        <h1>Game Library Tracker</h1>
+      </header>
+
+      <main>
+        <GameCollection />
+        <GameProgress />
+      </main>
+
+      <footer>
+        <p>Project by: Sukhtaj and Beerdavinder</p>
+      </footer>
+    </div>
+  );
 }
 
-export default App
+export default App;
+
