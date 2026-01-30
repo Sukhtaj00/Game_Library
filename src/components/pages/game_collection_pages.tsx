6c6d9989363cb9eea8c2eed37db4a2796039185c
Sukhtaj Dhillon <you@example.com>
2026-01-30 02:25:11 -0600

    created multi page navigation

---
+++ src/components/pages/game_collection_pages.tsx
@@ -0,0 +1,7 @@
+import GameCollection from "../game_collection/game_collection";
+
+function GameCollectionPage() {
+  return <GameCollection />;
+}
+
+export default GameCollectionPage;
---
+++ src/components/pages/game_progress_pages.tsx
@@ -0,0 +1,7 @@
+import GameProgress from "../game_progress/game_progress";
+
+function GameProgressPage() {
+  return <GameProgress />;
+}
+
+export default GameProgressPage;
