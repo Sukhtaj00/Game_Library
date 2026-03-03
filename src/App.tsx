c4401873c09f3c0b8e63dc4671007919127c025f
Sukhtaj Dhillon <you@example.com>
2026-03-02 22:05:13 -0600

    updated the game collection page and app.tsx as well

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,15 @@
-import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/layout/layout";
 import GameCollectionPage from "./components/pages/game_collection_pages";
 import GameProgressPage from "./components/pages/game_progress_pages";
-import "./App.css"
-
-export type Game = {
-  title: string;
-  completion: number;
-};
+import "./App.css";
 
 function App() {
-  const [games, setGames] = useState<Game[]>([
-    { title: "God of War", completion: 100 },
-    { title: "Halo Infinite", completion: 60 },
-  ]);
-
-  const totalGames = games.length;
-
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route
-          path="collection"
-          element={
-            <GameCollectionPage
-              games={games}
-              setGames={setGames}
-              totalGames={totalGames}
-            />
-          }
-        />
-        <Route
-          path="progress"
-          element={
-            <GameProgressPage
-              games={games}
-              setGames={setGames}
-              totalGames={totalGames}
-            />
-          }
-        />
+        <Route path="collection" element={<GameCollectionPage />} />
+        <Route path="progress" element={<GameProgressPage />} />
       </Route>
     </Routes>
   );
